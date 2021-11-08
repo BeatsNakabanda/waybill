7cad2255b733f515137826d197accee4d05469c0
beatrice <beatrice@laboremus.no>
2021-11-08 09:02:06 +0300

    Modified server file

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const jsonServer = require("json-server");
+const favicon = require('express-favicon');
 const server = jsonServer.create();
 const router = jsonServer.router("src/data/db.json");
 const middlewares = jsonServer.defaults({ static: "./build"});
 const port = process.env.PORT || 3000;
 
+server.use(favicon(__dirname + '/public/favicon.png'))
 server.use(middlewares);
 server.use(router);
 
